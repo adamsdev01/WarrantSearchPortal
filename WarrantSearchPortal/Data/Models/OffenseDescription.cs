5264646c1172a06af5fb3a380a4b27d1b28f1e21
TA <tadams03@mail.roosevelt.edu>
2023-11-08 20:53:42 -0600

    Scaffold db

---
+++ WarrantSearchPortal/Data/Models/OffenseDescription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarrantSearchPortal.Data.Models;
+
+public partial class OffenseDescription
+{
+    public int OffenseDescriptionId { get; set; }
+
+    public string OffenseName { get; set; }
+
+    public string OffenseDefinition { get; set; }
+
+    public string CurrentFlag { get; set; }
+}
---
+++ WarrantSearchPortal/Data/Models/WarrantPortalDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WarrantSearchPortal.Data.Models;
+
+public partial class WarrantPortalDbContext : DbContext
+{
+    public WarrantPortalDbContext()
+    {
+    }
+
+    public WarrantPortalDbContext(DbContextOptions<WarrantPortalDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<OffenseDescription> OffenseDescriptions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OffenseDescription>(entity =>
+        {
+            entity.ToTable("OffenseDescription");
+
+            entity.Property(e => e.OffenseDescriptionId).ValueGeneratedNever();
+            entity.Property(e => e.CurrentFlag)
+                .HasMaxLength(1)
+                .IsUnicode(false);
+            entity.Property(e => e.OffenseDefinition)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.OffenseName)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
