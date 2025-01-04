64d4e4539e003f862ef9a0ed997b6c2762de0b69
TA <tadams03@mail.roosevelt.edu>
2025-01-04 17:01:44 -0600

    language update

---
+++ WarrantSearchPortal/Data/Models/SearchWarrant.cs
@@ -0,0 +1,11 @@
+﻿namespace WarrantSearchPortal.Data.Models
+{
+    public class SearchWarrant
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string WarrantType { get; set; }
+        public bool Arrestable { get; set; }
+
+    }
+}
